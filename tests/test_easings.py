ec5aab6f78f424c9607654378180613e2fd8069f
Dave Jones <dave@waveform.org.uk>
2018-06-25 00:31:19 +0100

    Back-port isclose for easings tests

--- tests/test_easings.py
+++ tests/test_easings.py
@@ -35,12 +35,31 @@ from __future__ import (
 )
 
 from itertools import tee
-from math import isclose
 
 import pytest
 
 from pisense import *
 
+try:
+    from math import isclose
+except ImportError:
+    import cmath
+    # Back-ported from python 3.5; see
+    # github.com/PythonCHB/close_pep/blob/master/is_close.py for original
+    # implementation
+    def isclose(a, b, rel_tol=1e-9, abs_tol=0.0):
+        if rel_tol < 0.0 or abs_tol < 0.0:
+            raise ValueError('error tolerances must be non-negative')
+        if a == b: # fast-path for exact equality
+            return True
+        if cmath.isinf(a) or cmath.isinf(b):
+            return False
+        diff = abs(b - a)
+        return (
+            (diff <= abs(rel_tol * b)) or
+            (diff <= abs(rel_tol * a)) or
+            (diff <= abs_tol)
+            )
 try:
     from unittest import mock
 except ImportError:
